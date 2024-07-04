a8b7e5ce0af7bfc174bd95e48d6fdac7394956f3
Andrey Tyukachev <tyukachev2014@mail.ru>
2024-07-04 22:50:58 +0300

    added hash

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const cols = document.querySelectorAll(".column");
-const btn = document.querySelectorAll("button");
-const italic = document.querySelectorAll("i");
 
 document.addEventListener("keydown", (event) => {
-  event.preventDefault();
+  if (event.code.toLocaleLowerCase() === "space") {
+    event.preventDefault();
+  }
   if (event.code.toLocaleLowerCase() === "space") {
     setRandomColors();
   }
@@ -20,6 +20,8 @@ document.addEventListener("click", (event) => {
 
     node.classList.toggle("fa-lock-open");
     node.classList.toggle("fa-lock");
+  } else if (type === "copy") {
+    copyOnClick(event.target.textContent);
   }
 });
 
@@ -32,16 +34,41 @@ const generateRandomColor = () => {
   return "#" + color;
 };
 
-const setRandomColors = () => {
-  cols.forEach((col) => {
+const copyOnClick = (text) => {
+  return navigator.clipboard.writeText(text);
+};
+
+const setRandomColors = (isInitial) => {
+  const colors = isInitial ? getColorsFromHash() : [];
+
+  cols.forEach((col, index) => {
+    const isLocked = col.querySelector("i").classList.contains("fa-lock");
     const text = col.querySelector("h2");
     const btn = col.querySelector("button");
-    const randomColor = generateRandomColor();
+
+    const randomColor = isInitial
+      ? colors[index]
+        ? colors[index]
+        : generateRandomColor()
+      : generateRandomColor();
+
+    if (isLocked) {
+      colors.push(text.textContent);
+      return;
+    }
+
+    if (!isInitial) {
+      colors.push(randomColor);
+    }
+
     text.innerHTML = randomColor;
     col.style.background = randomColor;
+
     setTextColor(text, randomColor);
     setTextColor(btn, randomColor);
   });
+
+  updateColorsHash(colors);
 };
 
 const setTextColor = (text, color) => {
@@ -49,4 +76,22 @@ const setTextColor = (text, color) => {
   text.style.color = luminance > 0.5 ? "black" : "white";
 };
 
-setRandomColors();
+const updateColorsHash = (colors = []) => {
+  document.location.hash = colors
+    .map((col) => {
+      return col.toString().substring(1);
+    })
+    .join("-");
+};
+
+const getColorsFromHash = () => {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split("-")
+      .map((randomColor) => "#" + randomColor);
+  }
+  return [];
+};
+
+setRandomColors(true);
